7d5d6e05d6ab986079b316e2bb145c9597fda612
hansel.suarez <hansel.suarez@pragma.com.co>
2017-09-23 23:06:18 -0500

    Funcionalidad de la pantalla de agregar lista

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaItem, Lista } from '../../app/clases/index';
 
 @Component({
     selector: 'app-agregar',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AgregarComponent implements OnInit {
+
+    titulo: string;
+    item: string;
+
+    items: ListaItem[] = [];
+
     constructor() { }
 
     ngOnInit() { }
 
-    titulo: string;
-    item: string;
+    agregar() {
+        if (this.item === '' || this.item === undefined) {
+            return;
+        }
+
+
+        let item = new ListaItem();
+        item.nombre = this.item;
+
+        this.items.push(item);
+        this.item = '';
+    }
 }
