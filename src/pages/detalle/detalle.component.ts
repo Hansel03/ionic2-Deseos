ab7574b46be8108b974be9bfd7deca46543d5235
hansel.suarez <hansel.suarez@pragma.com.co>
2017-12-19 23:10:08 -0500

    Pantalla de detalle y sus funciones

--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Lista, ListaItem } from '../../app/clases/index';
+import { ListaDeseoService } from '../../app/services/lista-deseos.services';
+import { AlertController } from 'ionic-angular';
 
 @Component({
     selector: 'app-detalle',
@@ -9,11 +12,13 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DetalleComponent implements OnInit {
 
     idx: number;
-    lista: any;
+    lista: Lista;
 
     constructor(
         public navCtrl: NavController,
-        public navParams: NavParams) {
+        public navParams: NavParams,
+        public _listaDeseos: ListaDeseoService,
+        public alertCtrl: AlertController) {
 
         // Capturamos los enviados enviados por parametros
         // y los obtenemos con navParams y lo pasamos a una variable
@@ -22,4 +27,39 @@ export class DetalleComponent implements OnInit {
     }
 
     ngOnInit() { }
+
+    /**
+     * Actualizar el nombre de una lista seleccionada
+     * 
+     * @param {ListaItem} item 
+     * @memberof DetalleComponent.actualizar
+    / */
+    actualizar(item) {
+        item.completado = !item.completado;
+
+        this._listaDeseos.actualizarData();
+    }
+
+    borrarItem() {
+        let confirm = this.alertCtrl.create({
+            title: 'Borrar Lista',
+            message: '¿Está seguro que desea eliminar esta lista?',
+            buttons: [
+                {
+                    text: 'NO',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'SI',
+                    handler: () => {
+                        console.log('Agree clicked');
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
 }
